692b74514ddc0db80b257bc962722737b055868b
jdsteege <jdsteege@gmail.com>
2023-12-14 10:27:22 -0600

    Added navbar

---
+++ src/components/BucketList.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { useDatabase, useDatabaseList } from "vuefire";
+import { ref as dbRef, query, orderByChild } from "firebase/database";
+
+const db = useDatabase();
+const buckets = useDatabaseList(
+  query(dbRef(db, "/buckets"), orderByChild("order"))
+);
+</script>
+
+<template>
+  <ol>
+    <li v-for="bucket in buckets" :key="bucket.id">
+      <b>{{ bucket.id }}</b
+      >: {{ JSON.stringify(bucket) }}
+    </li>
+  </ol>
+
+  <ul>
+    <li v-for="i in 100" :key="i">{{ i }}</li>
+  </ul>
+</template>
---
+++ src/components/LandingPage.vue
@@ -0,0 +1,19 @@
+<script setup>
+import BucketList from "./BucketList.vue";
+import Navbar from "./Navbar.vue";
+</script>
+
+<template>
+  <Navbar>
+    <template v-slot:menu>
+      <li><a>Buckets</a></li>
+      <li><a>Transactions</a></li>
+    </template>
+
+    <template v-slot:content>
+      <div class="bg-base-200">
+        <BucketList></BucketList>
+      </div>
+    </template>
+  </Navbar>
+</template>
